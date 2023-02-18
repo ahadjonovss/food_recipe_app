613558d325e7101e65a189f3f7254b216af640b9
Asus <samandarahadjonovss@gmail.com>
2023-02-18 18:05:14 +0500

    api doing

---
+++ lib/data/models/api_model/food_category.dart
@@ -0,0 +1,7 @@
+class FoodCategory{
+  static const genericFoods = "generic-foods";
+  static const genericMeals = "generic-meals";
+  static const packedFoods = "packed-foods";
+  static const fastFoods = "fast-foods";
+
+}
